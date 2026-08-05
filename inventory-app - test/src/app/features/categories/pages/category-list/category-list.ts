340bace61da1f52c0ffcbf950e197c143d4ccb93
nguyentum1nh2007 <64006466+nguyentum1nh2007@users.noreply.github.com>
2026-08-05 16:22:33 +0700

    Delete inventory-app - test directory

--- inventory-app - test/src/app/features/categories/pages/category-list/category-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-category-list',
-  standalone: false,
-  templateUrl: './category-list.html',
-  styleUrl: './category-list.scss',
-})
-export class CategoryList {}
