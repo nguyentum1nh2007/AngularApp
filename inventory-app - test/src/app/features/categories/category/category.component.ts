1e1d62e563235804105b2acc62db7b07e78198c0
nguyentum1nh2007 <64006466+nguyentum1nh2007@users.noreply.github.com>
2026-08-05 16:27:16 +0700

    Thêm tính năng sửa xóa danh mục

---
+++ inventory-app - test/src/app/features/categories/category/category.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../category.service';
+import { Category } from '../../../models/category.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app-category',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './category.component.html'
+})
+export class CategoryComponent implements OnInit {
+    categories: Category[] = [];
+    newCategoryName = '';
+
+    editingId: string | null = null;
+    editingName = '';
+
+    constructor(private categoryService: CategoryService) { }
+
+    ngOnInit() {
+        this.loadData();
+    }
+
+    loadData() {
+        this.categories = this.categoryService.getAll();
+    }
+
+    addCategory() {
+        this.categoryService.add(this.newCategoryName);
+        this.newCategoryName = '';
+        this.loadData();
+    }
+
+    deleteCategory(id: string) {
+        if (confirm('Bạn có chắc muốn xóa danh mục này?')) {
+            this.categoryService.delete(id);
+            this.loadData();
+        }
+    }
+
+    startEdit(cat: Category) {
+        this.editingId = cat.id;
+        this.editingName = cat.name;
+    }
+
+    saveEdit() {
+        if (this.editingId) {
+            this.categoryService.update(this.editingId, this.editingName);
+            this.editingId = null;
+            this.editingName = '';
+            this.loadData();
+        }
+    }
+
+    cancelEdit() {
+        this.editingId = null;
+        this.editingName = '';
+    }
+}
---
+++ inventory-app - test/src/app/features/categories/pages/category-list/category-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-list',
+  standalone: false,
+  templateUrl: './category-list.html',
+  styleUrl: './category-list.scss',
+})
+export class CategoryList {}
